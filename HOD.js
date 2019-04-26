bec36863fdfbb8ecd1ee850b4e71cebd843870ec
seun jay <seunjay92@gmail.com>
2019-04-26 15:08:02 +0100

    Define both readAllAccounts and deleteAllAccounts method on the Hod constructor prototype

--- HOD.js
+++ HOD.js
@@ -12,4 +12,18 @@ Hod.prototype.constructor = Hod;
 let chukwu = new Hod('Chukwu Angela', 'chukwu@gmail.com', 4567);
 //console.log(chukwu.createAccount())
 
+Hod.prototype.readAllAccounts = function(){
+  if(db.lecturers.length > 0){
+    console.log(db.lecturers);
+    return 'These are all the available accounts';
+  } else return "No lecturer's account available"
+}
+
+Hod.prototype.deleteAllAccounts = function(){
+  if(db.lecturers.length){
+    db.lecturers.length = 0;
+    return 'All accounts have been successfully deleted'
+  } else return 'All accounts have been successfully deleted'
+}
+
 module.exports = Hod;
