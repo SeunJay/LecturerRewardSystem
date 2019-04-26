e65458ba7fe01bf487dcc9d9b6a8ae39a47e8139
seun jay <seunjay92@gmail.com>
2019-04-26 14:33:48 +0100

    Define Hod constructor, set its prototype to that of the Lecturer and setting its constructor back to itself

--- HOD.js
+++ HOD.js
@@ -2,3 +2,14 @@
 const db = require('./db');
 const Lecturer = require('./lecturer');
 
+function Hod(username, email, password){
+  Lecturer.call(this, username, email, password)
+};
+
+Hod.prototype = Object.create(Lecturer.prototype);
+Hod.prototype.constructor = Hod;
+
+let chukwu = new Hod('Chukwu Angela', 'chukwu@gmail.com', 4567);
+//console.log(chukwu.createAccount())
+
+module.exports = Hod;
