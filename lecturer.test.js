799dd0cce6fae0b592cb82002e6f5ec2a30701d7
seun jay <seunjay92@gmail.com>
2019-04-26 10:29:55 +0100

    Correct a description of test case

--- lecturer.test.js
+++ lecturer.test.js
@@ -12,4 +12,9 @@ describe('Lecturer functionalities', () =>{
     expect(femi).toEqual({username: 'Femi Ayoola', email: 'femi@gmail.com', password: 1155, studentScores: [50, 60, 67, 75]});
   });
 
+  test("Check to see that a lecturer has created his account", () => {
+    let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
+    expect(femi.createAccount()).toBe('Your account has been successfully created');
+  })
+
 })
