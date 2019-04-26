ad4f3b6c8cf0b353b807a045923c35651fd12e40
seun jay <seunjay92@gmail.com>
2019-04-26 10:57:26 +0100

    Add test case of another lecturer creating his account

--- lecturer.test.js
+++ lecturer.test.js
@@ -12,9 +12,19 @@ describe('Lecturer functionalities', () =>{
     expect(femi).toEqual({username: 'Femi Ayoola', email: 'femi@gmail.com', password: 1155, studentScores: [50, 60, 67, 75]});
   });
 
-  test("Check to see that a lecturer has created his account", () => {
+  test("Check to see that a lecturer Femi has created his account", () => {
     let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
     expect(femi.createAccount()).toBe('Your account has been successfully created');
+  });
+
+  // test("Check for the instance of the Lecturer Constructor", () => {
+  //   let ojo = new Lecturer("Wole Ojo", "ojo@gmail.com", 2234, [60, 70, 67, 75]);
+  //   expect(ojo).toEqual({username: 'Wole Ojo', email: 'ojo@gmail.com', password: 2234, studentScores: [60, 70, 67, 75]});
+  // });
+
+  test("Check to see that a lecturer Ojo has created his account", () => {
+    let ojo = new Lecturer("Wole Ojo", "ojo@gmail.com", 2234, [60, 70, 67, 75]);
+    expect(ojo.createAccount()).toBe('Your account has been successfully created');
   })
 
 })
