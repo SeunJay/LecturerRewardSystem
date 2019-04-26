91ca975b7e98dfa9ad6f8a62a25631e494a69c45
seun jay <seunjay92@gmail.com>
2019-04-26 11:06:03 +0100

    Add test case of a lecturer reading his account by ID

--- lecturer.test.js
+++ lecturer.test.js
@@ -17,14 +17,14 @@ describe('Lecturer functionalities', () =>{
     expect(femi.createAccount()).toBe('Your account has been successfully created');
   });
 
-  // test("Check for the instance of the Lecturer Constructor", () => {
-  //   let ojo = new Lecturer("Wole Ojo", "ojo@gmail.com", 2234, [60, 70, 67, 75]);
-  //   expect(ojo).toEqual({username: 'Wole Ojo', email: 'ojo@gmail.com', password: 2234, studentScores: [60, 70, 67, 75]});
-  // });
-
   test("Check to see that a lecturer Ojo has created his account", () => {
     let ojo = new Lecturer("Wole Ojo", "ojo@gmail.com", 2234, [60, 70, 67, 75]);
     expect(ojo.createAccount()).toBe('Your account has been successfully created');
-  })
+  });
+
+  test("check to see that a lecturer can read his account by ID", () =>{
+    let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
+    expect(femi.readYourAccount(1)).toEqual({id: 1, name: 'Femi Ayoola', email: 'femi@gmail.com', password: 1155, studentScores: [50, 60, 67, 75]})
+  });
 
 })
