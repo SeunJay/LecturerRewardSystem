324cada837b33b61139ccc2e92a8aa01144cbf7a
seun jay <seunjay92@gmail.com>
2019-04-26 09:55:13 +0100

    Add test case of Lecturer constructor being defined

--- lecturer.test.js
+++ lecturer.test.js
@@ -1,2 +1,9 @@
 const Lecturer = require('./lecturer');
 const db = require('./db');
+
+describe('Lecturer functionalities', () =>{
+  test('Check to see that the Lecturer constructor has been defined', () =>{
+    let femi = new Lecturer('Femi Ayola', 'femi@gmail.com', 1155, [50, 60, 67, 75]);
+    expect(femi).toBeDefined();
+  })
+})
