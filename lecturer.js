d745280385cbec19629554ac49552780551e3aa3
seun jay <seunjay92@gmail.com>
2019-04-26 10:32:39 +0100

    Define createAccount on Lecturer constructor prototype

--- lecturer.js
+++ lecturer.js
@@ -1,3 +1,4 @@
+const db = require('./db');
 
 function Lecturer(username, email, password, ...studentScores){
   this.username = username;
@@ -6,5 +7,28 @@ function Lecturer(username, email, password, ...studentScores){
   this.studentScores = studentScores[0]
 }
 
+
+Lecturer.prototype = {
+  constructor: Lecturer,
+
+  createAccount: function(){
+    let id = 0;
+    //let message = '';
+    if(db.lecturers.length){
+      id = db.lecturers[db.lecturers.length].id + 1
+    } else id = 1
+
+    db.lecturers.push({
+        id: id,
+        username: this.username,
+        email: this.email,
+        password: this.password,
+        studentScores: this.studentScores
+    });
+
+    console.log(db.lecturers);
+    return 'Your account has been successfully created'
+  }
+}
 
 module.exports = Lecturer
