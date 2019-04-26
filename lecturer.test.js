ebea04141d83e21009b2364ffc1cf1207308ea87
seun jay <seunjay92@gmail.com>
2019-04-26 10:09:52 +0100

    Add test case of the instance of the Lecturer constructor being instantiated

--- lecturer.test.js
+++ lecturer.test.js
@@ -3,7 +3,13 @@ const db = require('./db');
 
 describe('Lecturer functionalities', () =>{
   test('Check to see that the Lecturer constructor has been defined', () =>{
-    let femi = new Lecturer('Femi Ayola', 'femi@gmail.com', 1155, [50, 60, 67, 75]);
+    let femi = new Lecturer('Femi Ayoola', 'femi@gmail.com', 1155, [50, 60, 67, 75]);
     expect(femi).toBeDefined();
   })
+
+  test("Check for the instance of the Lecturer Constructor", () => {
+    let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
+    expect(femi).toEqual({username: 'Femi Ayoola', email: 'femi@gmail.com', password: 1155, studentScores: [50, 60, 67, 75]});
+  });
+
 })
