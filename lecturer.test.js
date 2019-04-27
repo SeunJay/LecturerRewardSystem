7edec32e1b7d32ec48e6c14fc301050001813bc7
seun jay <seunjay92@gmail.com>
2019-04-27 15:20:18 +0100

    Add test case of an HOD instance determining whether a lecturer is deserving of a reward

--- lecturer.test.js
+++ lecturer.test.js
@@ -5,15 +5,24 @@ const Hod = require('./HOD');
 let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
 let ojo = new Lecturer("Wole Ojo", "ojo@gmail.com", 2234, [60, 70, 67, 75]);
 
+
 let chukwu = new Hod('Chukwu Angela', 'chukwu@gmail.com', 4567);
 
 describe('Lecturer functionalities', () =>{
   test('Check to see that the Lecturer constructor has been defined', () =>{
     expect(femi).toBeDefined();
+  });
+
+  test('Check to see that the Lecturer constructor has been defined', () =>{
+    expect(ojo).toBeDefined();
   })
 
   test("Check for the instance of the Lecturer Constructor", () => {
     expect(femi).toEqual({username: 'Femi Ayoola', email: 'femi@gmail.com', password: 1155, studentScores: [50, 60, 67, 75]});
+  });
+
+  test("Check for the instance of the Lecturer Constructor", () => {
+    expect(ojo).toEqual({username: 'Wole Ojo', email: 'ojo@gmail.com', password: 2234, studentScores: [60, 70, 67, 75]});
   });
 
   test("Check to see that a lecturer Femi has created his account", () => {
@@ -24,48 +33,57 @@ describe('Lecturer functionalities', () =>{
     expect(ojo.createAccount()).toBe('Your account has been successfully created');
   });
 
+
+  
+
   test("check to see that a lecturer can read his account by ID", () =>{
-    let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
+    //let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
     expect(femi.readYourAccount(1)).toEqual({id: 1, username: 'Femi Ayoola', email: 'femi@gmail.com', password: 1155, studentScores: [50, 60, 67, 75]})
   });
 
   test("check to see that a lecturer can read his account by ID", () =>{
-    let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
+    //let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
+    expect(ojo.readYourAccount(2)).toEqual({id: 2, username: 'Wole Ojo', email: 'ojo@gmail.com', password: 2234,studentScores: [60, 70, 67, 75]})
+  });
+
+
+  test("should return an error if the the data type of what is passed in is not a number", () =>{
+    //let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
     expect(femi.readYourAccount('1')).toEqual('Invalid Input: Please enter an integer')
   });
 
-  test("check to see that a lecturer can read his account by ID", () =>{
-    let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
+  test("should return an error if an invalid ID is entered", () =>{
+    //let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
     expect(femi.readYourAccount(10)).toEqual('No account found')
   });
 
   test("check to see that a lecturer can read his account by ID", () =>{
-    let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
+    //let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
     expect(femi.readYourAccount(10)).toEqual('No account found')
   });
 
   test("check to see that a lecturer can read his account by ID", () =>{
-    let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
-    expect(femi.updateYourAccount('Femi Ayoola', 'Femzy', 1155, 1115, 'femi@gmail.com', [60, 70, 65, 71])).toEqual('Your account has been successfully updated')
+    //let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
+    expect(femi.updateYourAccount('Femi Ayoola', 'Femzy', 1155, 1116, 'femi@gmail.com', [50, 60, 67, 75])).toEqual('Your account has been successfully updated')
   });
 
   test("Should return am error message if a wrong username or password is passed in", () =>{
-    let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
-    expect(femi.updateYourAccount('Femi Aya', 'Femzy', 1155, 1115, 'femi@gmail.com', [60, 70, 65, 71])).toEqual('You entered either the wrong username or password')
+    //let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
+    expect(femi.updateYourAccount('Femi Aya', 'Femzy', 1157, 1115, 'femi@gmail.com', [50, 60, 67, 75])).toEqual('You entered either the wrong username or password')
   });
 
   test("Check to see that a registered lecturer can get his student's scores", () =>{
-    let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
-    expect(femi.getYourStudentScores(1)).toEqual(db.lecturers[1].studentScores)
+    //let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1156, [50, 60, 67, 75]);
+    expect(femi.getYourStudentScores(1)).toEqual([50, 60, 67, 75])
   });
 
   test("Return an error message if the data type of what is passed in is not a number", () =>{
-    let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
+    //let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
     expect(femi.getYourStudentScores('1')).toEqual('Invalid Input: Please enter an integer')
   });
 
-  test("Return an error message if the data type of what is passed in is not a number", () =>{
-    let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
+  test("Return an error message if invalid lecturer ID is passed in", () =>{
+    //let femi = new Lecturer("Femi Ayoola", "femi@gmail.com", 1155, [50, 60, 67, 75]);
     expect(femi.getYourStudentScores(10)).toEqual('No account found')
   });
 
@@ -87,27 +105,40 @@ describe('HOD functionalities', () =>{
     expect(chukwu).toBeDefined();
   });
 
-  test("Check for the instance of the Hod Constructor", () =>{
-    expect(chukwu).toEqual({username: 'Chukwu Angela', email: 'chukwu@gmail.com', password: 4567})
-  });
+  // test("Check for the instance of the Hod Constructor", () =>{
+  //   expect(chukwu).toEqual({username: 'Chukwu Angela', email: 'chukwu@gmail.com', password: 4567})
+  // });
+  
 
   test("Check to see that Hod Chukwu can create her account", () =>{
     expect(chukwu.createAccount()).toEqual('Your account has been successfully created')
   });
 
-  test("Check to see that Hod Chukwu can create her account", () =>{
+  test("Check to see that Hod Chukwu can read all lecturer's account", () =>{
     expect(chukwu.readAllAccounts()).toEqual('These are all the available accounts');
   });
 
-  test("Check to see that Hod Chukwu can create her account", () =>{
+  test("Check to see that Hod Chukwu can determine whether a lecturer is deserving of a reward", () =>{
+    expect(chukwu.isDeservingOfReward(1)).toEqual('Try harder');
+  });
+
+  test("Check to see that Hod Chukwu can determine whether a lecturer is deserving of a reward", () =>{
+    expect(chukwu.isDeservingOfReward(2)).toEqual('You deserve some accolades');
+  });
+
+  test("Check to see that Hod Chukwu can delete all lecturer's account", () =>{
     expect(chukwu.deleteAllAccounts()).toEqual('All accounts have been successfully deleted');
   });
 
-  test("Check to see that Hod Chukwu can create her account", () =>{
+  test("Check to see that Hod Chukwu can read all lecturer's account", () =>{
     expect(chukwu.readAllAccounts()).toEqual("No lecturer's account available");
   });
 
-  test("Check to see that Hod Chukwu can create her account", () =>{
+  test("Check to see that Hod Chukwu can delete all lecturer's account", () =>{
     expect(chukwu.deleteAllAccounts()).toEqual('All accounts have been successfully deleted');
   });
+
+  
+
+
 })
