49b3e41bc322ebb397ecd7eeaec14f198699dfa9
seun jay <seunjay92@gmail.com>
2019-04-27 15:25:22 +0100

    Add status property for Hod constructor

--- HOD.js
+++ HOD.js
@@ -3,18 +3,18 @@ const db = require('./db');
 const Lecturer = require('./lecturer');
 
 function Hod(username, email, password){
-  Lecturer.call(this, username, email, password)
+  Lecturer.call(this, username, email, password);
+  this.status = "admin";
+  this.studentScores = null
+  console.log(this)
 };
 
 Hod.prototype = Object.create(Lecturer.prototype);
 Hod.prototype.constructor = Hod;
 
-let chukwu = new Hod('Chukwu Angela', 'chukwu@gmail.com', 4567);
-//console.log(chukwu.createAccount())
 
 Hod.prototype.readAllAccounts = function(){
   if(db.lecturers.length > 0){
-    console.log(db.lecturers);
     return 'These are all the available accounts';
   } else return "No lecturer's account available"
 }
@@ -24,6 +24,19 @@ Hod.prototype.deleteAllAccounts = function(){
     db.lecturers.length = 0;
     return 'All accounts have been successfully deleted'
   } else return 'All accounts have been successfully deleted'
+};
+Hod.prototype.isDeservingOfReward = function(lecturerID){
+  let averageThresholdScore = 65;
+  let normalScore = Hod.prototype.getStudentAverageScore(lecturerID);
+  return (normalScore >= averageThresholdScore) ? 'You deserve some accolades': 'Try harder'
 }
+
+
+let chukwu = new Hod('Chukwu Angela', 'chukwu@gmail.com', 4567);
+console.log(chukwu.createAccount());
+// console.log(chukwu.readAllAccounts());
+// console.log(chukwu.getStudentAverageScore(2))
+// console.log(chukwu.isDeservingOfReward(2));
+// console.log(chukwu.isDeservingOfReward(1))
 
 module.exports = Hod;
